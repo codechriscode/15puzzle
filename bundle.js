8c258e3f9d106e91dbf7da3b004887e455ba089a
Chris Sousa <chrisqcs@tuta.io>
2022-02-21 11:08:56 -0300

    Basic types and logic

--- bundle.js
+++ bundle.js
@@ -1,1 +1,53 @@
 "use strict";
+const GAME_SIZE = 16;
+let gameState = [];
+function generate() {
+    //Generate beads 0-15
+    let beadSequence = [];
+    for (let i = 0; i < GAME_SIZE; i++) {
+        let span = document.createElement("span");
+        //0 is always empty space
+        if (i) {
+            let h2 = document.createElement("h2");
+            h2.innerText = `${i}`;
+            span.appendChild(h2);
+            span.classList.add("bead");
+        }
+        span.classList.add("position");
+        let newBead = {
+            span: span,
+            id: i,
+            moveable: null,
+        };
+        beadSequence.push(newBead);
+    }
+    return beadSequence;
+}
+function shuffleBeads(beadSequence) {
+    let shuffledGrid = [];
+    for (let i = 0; i < GAME_SIZE; i++) {
+        let randomIndex = randomInRange(0, beadSequence.length);
+        shuffledGrid.push(beadSequence.splice(randomIndex, 1)[0]);
+    }
+    return shuffledGrid;
+}
+function render(grid) {
+    let gridSpace = document.getElementById("grid");
+    if (gridSpace != null) {
+        gridSpace.innerHTML = '';
+        for (let i = 0; i < GAME_SIZE; i++) {
+            console.log(i);
+            gridSpace === null || gridSpace === void 0 ? void 0 : gridSpace.appendChild(grid[i].span);
+        }
+    }
+}
+function newGame() {
+    render(shuffleBeads(generate()));
+}
+/*Helpers*/
+//Returns in [min,max)
+function randomInRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
--- src/game.ts
+++ src/game.ts
@@ -1,1 +1,75 @@
-alert("Game's on!")
+const GAME_SIZE = 16;
+
+type PossibleMove = "up" | "down" | "left" | "right" | null;
+
+type Bead = {
+  span: HTMLSpanElement;
+  id: number;
+  moveable: PossibleMove;
+}
+
+type Grid = Array<Bead>
+
+type GameState = Array<Grid>
+let gameState: GameState = [];
+
+function generate(): Grid {
+  //Generate beads 0-15
+  let beadSequence: Grid = [];
+  for(let i=0; i<GAME_SIZE; i++) {
+
+    let span = document.createElement("span");
+    //0 is always empty space
+    if(i) {
+      let h2 = document.createElement("h2");
+      h2.innerText = `${i}`
+      span.appendChild(h2);
+      span.classList.add("bead");
+    }
+    span.classList.add("position");
+
+    let newBead: Bead = {
+      span: span,
+      id: i,
+      moveable: null,
+    }
+
+    beadSequence.push(newBead);
+  }
+
+  return beadSequence;
+}
+
+function shuffleBeads(beadSequence: Grid): Grid {
+  let shuffledGrid: Grid = [];
+  for(let i=0; i<GAME_SIZE; i++){
+    let randomIndex = randomInRange(0,beadSequence.length);
+    shuffledGrid.push(beadSequence.splice(randomIndex,1)[0]);
+  }
+  return shuffledGrid;
+}
+
+function render(grid: Grid): void {
+  let gridSpace = document.getElementById("grid");
+  if(gridSpace != null) {
+    gridSpace.innerHTML = '';
+    for(let i=0; i<GAME_SIZE; i++){
+      console.log(i)
+      gridSpace?.appendChild(grid[i].span);
+    }
+  }
+}
+
+function newGame() {
+  render(shuffleBeads(generate()));
+}
+
+
+/*Helpers*/
+//Returns in [min,max)
+function randomInRange(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
