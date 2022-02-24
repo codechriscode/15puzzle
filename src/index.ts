b73e64c64b70e1178be7920d2ceb727f79a9979a
Chris Sousa <chrisqcs@tuta.io>
2022-02-24 15:40:10 -0300

    "Undo" feature functionality

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ function generate(): Grid {
 }
 
 // Creates spans and sets div#playGround
-function render(grid: Grid) {
+function render(grid: Grid = lastState()) {
   let rendered: DocumentFragment = document.createDocumentFragment();
   for (let i = 0; i < grid.length; i++) {
     let span = document.createElement("span");
@@ -44,7 +44,7 @@ function render(grid: Grid) {
 }
 
 // Checks click location and moves if movable
-function handleClick(e: any, grid: Grid = gameState[gameState.length -1]) {
+function handleClick(e: any, grid: Grid = lastState()) {
   let selId = parseInt(e.currentTarget.id);
   let indexOfSel = grid.indexOf(selId);
   let movability = isMovable(indexOfSel);
@@ -52,9 +52,9 @@ function handleClick(e: any, grid: Grid = gameState[gameState.length -1]) {
   //Confirm move: create new history that
   //will be changed and rendered
   if(movability) {
-    setState([...gameState[gameState.length-1]])
+    setState([...lastState()])
     finishMove(indexOfSel, movability)
-    render(gameState[gameState.length-1]);
+    render();
   }
 }
 
@@ -62,7 +62,7 @@ function handleClick(e: any, grid: Grid = gameState[gameState.length -1]) {
 function finishMove(
   selPos: number,
   direction: PossibleMove,
-  grid: Grid = gameState[gameState.length - 1]
+  grid: Grid = lastState()
 ) {
   let destPos: number = selPos+(moveBy(direction))
   // If it has not found the current empty position,
@@ -74,9 +74,22 @@ function finishMove(
   [grid[selPos], grid[destPos]] = [grid[destPos], grid[selPos]]
 }
 
-let newGame = generate();
-setState(newGame);
-render(newGame);
+function undo(){
+  gameState.pop();
+  render();
+}
+
+function start() {
+  let undoBtn = document.getElementById("undo") as HTMLElement;
+  undoBtn.onclick = undo;
+  
+  let newGame = generate();
+  setState(newGame);
+  render();
+}
+
+start();
+
 
 /*HELPERS *****************************************/
 //Returns in [min,max)
@@ -131,4 +144,7 @@ function moveBy(direction: PossibleMove) {
   }
 }
 
+function lastState() {
+  return gameState[gameState.length - 1]
+}
 // function getNextMovable(grid: Grid) {}
